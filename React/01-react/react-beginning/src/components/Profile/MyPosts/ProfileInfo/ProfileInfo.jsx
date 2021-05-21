bf9597008e7f94629646d1c21304969a6c5dc015
gelserkirchen <gelserkirchen@mail.ru>
2021-05-21 06:56:18 +0300

    change class to hooks to change status

--- React/01-react/react-beginning/src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
+++ React/01-react/react-beginning/src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './ProfileInfo.module.css'
 import Preloader from '../../../MultiComponents/Preloader';
 import ProfileStatus from './ProfileStatus'
+import ProfileStatusWithHook from './ProfileStatusWithHooks';
 
 const ProfileInfo = (props) => {
 
@@ -16,7 +17,7 @@ const ProfileInfo = (props) => {
       <div><img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages4.fanpop.com%2Fimage%2Fphotos%2F21800000%2FBanner-P-summer448-21813774-2560-1024.jpg&f=1&nofb=1" alt=""/></div>
       <div className={styles.avatar}><img src={props.profile.photos.large} alt=''/></div>
       <div>{props.profile.fullName}</div>
-      <div><ProfileStatus status={props.status} updateProfileStatus={props.updateProfileStatus}/></div>
+      <div><ProfileStatusWithHook status={props.status} updateProfileStatus={props.updateProfileStatus}/></div>
     </div>
   )
 }
